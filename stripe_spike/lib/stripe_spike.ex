481cec281bf8e47d72ffaa2c4a87dd01677a6a5d
Benjamin Denton <bdenton@heroku.com>
2022-01-27 09:18:07 -0500

    initial project setup

---
+++ stripe_spike/lib/stripe_spike.ex
@@ -0,0 +1,18 @@
+defmodule StripeSpike do
+  @moduledoc """
+  Documentation for `StripeSpike`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StripeSpike.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ stripe_spike/test/stripe_spike_test.exs
@@ -0,0 +1,8 @@
+defmodule StripeSpikeTest do
+  use ExUnit.Case
+  doctest StripeSpike
+
+  test "greets the world" do
+    assert StripeSpike.hello() == :world
+  end
+end
